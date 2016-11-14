fc270cf9e8790d986c8413bb3bd45b5b25f13263
revmc <shaque9000@gmail.com>
2016-11-13 23:50:19 -0500

    Reading US dollar value from the input field

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,15 @@
 define(['currencyConverter', 'domUpdater'], function(currencyConverter, domUpdater) {
 
-	domUpdater.insertText('lbs', currencyConverter.convertCurrency(100));
+	function updatePage() {
+		domUpdater.insertText('lbs', currencyConverter.convertCurrency(100));	
+	}
 	
-	console.log('$' + currencyConverter.convertCurrency(100));
+	var dollarsInputField = document.getElementById('dollars');
+
+	dollarsInputField.addEventListener('change', function(e) {
+		domUpdater.insertText('lbs', currencyConverter.convertCurrency(e.target.value));
+	})
+
+	var inputValue = dollarsInputField.value;
 
 })
--- scripts/domUpdater.js
+++ scripts/domUpdater.js
@@ -3,9 +3,11 @@ define(function() {
 	return {
 
 		insertText: function(id, text) {
+
 			var element = document.getElementById(id);
-			console.log('element : ', element);
-			element.appendChild(document.createTextNode(text));
+
+			element.innerHTML = text;
+
 		}
 
 	}
